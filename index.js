55181ab3a067e312fd5870bf3c2e183ddae48d5e
Jamal Barnes <mavjbarnes@gmail.com>
2017-08-27 15:57:43 -0700

    initial setup

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+
+const express = require('express')
+const expressGraphQL = require('express-graphql')
+const schema = require('./schema')
+const webpackMiddleware = require('webpack-dev-middleware')
+const webpack = require('webpack')
+const webpackConfig = require('./webpack.config.js')
+
+const port = process.env.PORT || 4000
+
+const app = express()
+
+app.use('/graphql', expressGraphQL({
+  schema,
+  graphiql: true
+}))
+
+app.use(webpackMiddleware(webpack(webpackConfig)))
+
+app.listen(port, (error) => {
+  if (error) {
+    console.error(error) // eslint-disable-line no-console
+  } else {
+    console.info(`==> 🌎  Listening on port ${ port }. Open up http://localhost:${ port }/ in your browser.`) // eslint-disable-line no-console
+  }
+})
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const webpack = require('webpack'); // eslint-disable-line no-unused-vars
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.jsx$/,
+        use: 'eslint',
+        exclude: /node_modules/
+      },
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'client/index.html'
+    })
+  ]
+};
