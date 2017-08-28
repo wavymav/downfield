a1fbfc181371492ee7ac8672cf0138ab1c985080
Jamal Barnes <mavjbarnes@gmail.com>
2017-08-27 19:57:38 -0700

    adds mutations for player

---
+++ schema/mutations/index.js
@@ -0,0 +1,88 @@
+require('dotenv').config()
+
+const axios = require('axios')
+const graphql = require('graphql')
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLID,
+  GraphQLNonNull
+} = graphql
+const PlayerType = require('./PlayerType')
+
+const ApiUrl = process.env.API_URL
+
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    createPlayer: {
+      type: PlayerType,
+      args: {
+        firstName: { type: new GraphQLNonNull(GraphQLString) },
+        lastName: { type: new GraphQLNonNull(GraphQLString) },
+        jerseyNumber: { type: new GraphQLNonNull(GraphQLInt) },
+        position: { type: new GraphQLNonNull(GraphQLString) },
+        team: { type: new GraphQLNonNull(GraphQLString) }
+      },
+      resolve: (parentValue, {
+        firstName,
+        lastName,
+        jerseyNumber,
+        position,
+        team
+      }) => (
+        axios
+          .post(`${ ApiUrl }/players`, {
+            firstName,
+            lastName,
+            jerseyNumber,
+            position,
+            team
+          })
+          .then(({ data }) => data)
+      )
+    },
+    editUser: {
+      type: PlayerType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLID) },
+        firstName: { type: new GraphQLNonNull(GraphQLString) },
+        lastName: { type: new GraphQLNonNull(GraphQLString) },
+        jerseyNumber: { type: new GraphQLNonNull(GraphQLInt) },
+        position: { type: new GraphQLNonNull(GraphQLString) },
+        team: { type: new GraphQLNonNull(GraphQLString) }
+      },
+      resolve: (parentValue, {
+        id,
+        firstName,
+        lastName,
+        jerseyNumber,
+        position,
+        team
+      }) => (
+        axios
+          .patch(`${ ApiUrl }/players/${ id }`, {
+            id,
+            firstName,
+            lastName,
+            jerseyNumber,
+            position,
+            team
+          })
+          .then(({ data }) => data)
+        )
+    },
+    deletePlayer: {
+      type: PlayerType,
+      args: { type: new GraphQLNonNull(GraphQLID) },
+      resolve: (parentValue, { id }) => (
+        axios
+          .delete(`${ ApiUrl }/players/${ id }`)
+          .then(({ data }) => data)
+      )
+    }
+  }
+});
+
+module.exports = mutation
