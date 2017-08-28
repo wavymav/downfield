426037b56e920520fc1b5f627c176132cab17a76
Jamal Barnes <mavjbarnes@gmail.com>
2017-08-28 12:30:12 -0700

    creates loading spinner component

---
+++ src/components/LoadingSpinner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const LoadingSpinner = () => (
+  <div className='m-loding-spinner'>
+    <div className='m-loding-spinner__inner'>
+      <span className='m-loding-spinner__dot'></span>
+      <span className='m-loding-spinner__dot'></span>
+      <span className='m-loding-spinner__dot'></span>
+    </div>
+  </div>
+)
